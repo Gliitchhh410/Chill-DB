40f265f75ff1cbd2a655d9720e7b400f6197394d
Gliitchhh410 <ahmedmounir532@gmail.com>
2026-02-13 13:31:22 +0200

    refactor: using bufio for writing WAL

--- v2/internal/db/wal.go
+++ v2/internal/db/wal.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"bufio"
 	"encoding/binary"
 	"os"
 	"sync"
@@ -11,9 +12,10 @@ import (
 // Recovery: If we crash, we just read this file from top to bottom on restart to rebuild the MemTable.
 
 type WAL struct {
-	file *os.File
-	path string
-	mu   sync.Mutex
+	file   *os.File
+	path   string
+	writer *bufio.Writer
+	mu     sync.Mutex
 }
 
 func NewWAL(path string) (*WAL, error) {
@@ -24,7 +26,7 @@ func NewWAL(path string) (*WAL, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &WAL{file: f, path: path}, nil
+	return &WAL{file: f, path: path, writer: bufio.NewWriterSize(f, 4096)}, nil
 }
 func (w *WAL) Truncate() error {
 	w.mu.Lock()
@@ -42,30 +44,41 @@ func (w *WAL) Truncate() error {
 	// 3. Re-open the file for appending
 	var err error
 	w.file, err = os.OpenFile(w.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	return err
+	if err != nil {
+		return err
+	}
+	w.writer.Reset(w.file)
+	return nil
 }
 
 func (w *WAL) Append(key string, value []byte) error {
 	w.mu.Lock()
 	defer w.mu.Unlock()
 
-	err := binary.Write(w.file, binary.LittleEndian, int32(len(key))) // Why binary.LittleEndian? Computers store numbers in different ways (big-endian vs little-endian). We choose one standard so that if you move the file to a different computer, it can still be read.
+	err := binary.Write(w.writer, binary.LittleEndian, int32(len(key))) // Why binary.LittleEndian? Computers store numbers in different ways (big-endian vs little-endian). We choose one standard so that if you move the file to a different computer, it can still be read.
 	if err != nil {
 		return err
 	}
-	err = binary.Write(w.file, binary.LittleEndian, int32(len(value))) // Why int32? We use a fixed size (4 bytes) for the length so the reader knows exactly how many bytes to read next.
+	err = binary.Write(w.writer, binary.LittleEndian, int32(len(value))) // Why int32? We use a fixed size (4 bytes) for the length so the reader knows exactly how many bytes to read next.
 	if err != nil {
 		return err
 	}
-	_, err = w.file.Write([]byte(key))
+	_, err = w.writer.WriteString(key)
 	if err != nil {
 		return err
 	}
-	_, err = w.file.Write(value)
+	_, err = w.writer.Write(value)
 	if err != nil {
 		return err
 	}
-	w.file.Sync()
+	//FLUSH: Push the buffer to the OS Kernel (1 System Call)
+	if err := w.writer.Flush(); err != nil {
+		return err
+	}
+	// (2 System Call)
+	if err := w.file.Sync(); err != nil {
+		return err
+	}
 	return nil
 }
 
